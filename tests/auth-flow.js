256d6f201a9f5e81fea37b7b7327d6389e5f8bcb
Mdjurdj1 <github email address>
2017-05-06 20:28:35 -0400

    Integrate nightwatch with first signup test

---
+++ tests/auth-flow.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  'get to signup page': (client) => {
+
+    client
+      .url(client.launchUrl)
+      .waitForElementVisible('.navbar', 1000)
+      .click('a[href="/signup"]')
+
+    client.assert.urlContains('signup')
+  },
+
+  'signup user and redirect to cart': (client) => {
+    
+  }
+}
