325c205644d1a117ed2bdc4a876301524d200093
Mdjurdj1 <github email address>
2017-05-08 12:22:13 -0400

    Converted Home page to react component

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { Col, Grid, Row } from 'react-bootstrap'
+import React, {Component} from 'react'
+import { Col, Grid, Row, Button } from 'react-bootstrap'
 import './style.css'
 
-const Home = () =>
+class Home extends Component {
+render() {
+  return (
+      <Grid>
+        <Row>
+          <Col xs={12} md={8} >
+            <h1>Welcome to <span id="mini-tube">Mini-Tube!</span></h1>
+            <p className="text">A simple, minimally featured Youtube Browser.</p>
+          </Col>
 
-<Grid>
-  <Row>
-    <Col xs={12} md={8} >
-      <h1>Welcome to <span id="mini-tube">Mini-Tube!</span></h1>
-      <p className="text">A simple, minimally featured Youtube Browser.</p>
-    </Col>
-
-    <Col xs={6} md={4} >
-      <h3>Are you new here?</h3>
-      <p className="text">Sign up!</p>
-    </Col>
-  </Row>
-</Grid>
-
+          <Col xs={6} md={4} >
+            <h3>Are you new here?</h3>
+            <Button bsStyle="primary" bsSize="large" block onClick={e => this.props.history.push("/signup")} >Sign Up</Button>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
 
 export default Home
