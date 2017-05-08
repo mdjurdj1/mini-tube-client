6c093e4d0291773d8081236832a48c091ce719ce
Mdjurdj1 <github email address>
2017-05-07 20:53:15 -0400

    what

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
+import './style.css'
 
-const Home = () => <div>Home</div>
+const Home = () =>
+
+<div>
+<h1>GAME OF JONES</h1>
+<img id="hey" src='http://media.wwg.com/2017/04/marvel-vs-capcom-infinite-8-207902.jpg' />
+</div>
+
 
 
 export default Home
