3cdb400b206472af8929d5201b3f4d5b935d13dd
Mdjurdj1 <github email address>
2017-05-08 12:10:03 -0400

    Added features to homepage, as well as styling

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Col, Grid, Row } from 'react-bootstrap'
 import './style.css'
 
 const Home = () =>
 
-<div>
-<h1>THIS IS DEFINITELY AN APPLICATION</h1>
-<img id="hey" src='http://vignette2.wikia.nocookie.net/degrassi/images/4/4c/Toby-the-office-nbc.jpg/revision/latest?cb=20130910201716' alt="sadness"/>
-</div>
+<Grid>
+  <Row>
+    <Col xs={12} md={8} >
+      <h1>Welcome to <span id="mini-tube">Mini-Tube!</span></h1>
+      <p className="text">A simple, minimally featured Youtube Browser.</p>
+    </Col>
 
+    <Col xs={6} md={4} >
+      <h3>Are you new here?</h3>
+      <p className="text">Sign up!</p>
+    </Col>
+  </Row>
+</Grid>
 
 
 export default Home
