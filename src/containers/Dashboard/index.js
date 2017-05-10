2210c03750b99b82484b73e6fb1e2ff429e2f338
Mdjurdj1 <github email address>
2017-05-10 00:58:47 -0400

    Added dashboard view and styling

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,6 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {Row, Col} from 'react-bootstrap'
+import { connect } from 'react-redux'
+import './dashboard.css'
 
-const Dashboard = () => <div>Welcome, you're logged in!</div>
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <div className="banner">
+        <Row>
+          <Col xs={12} md={12} id="bufferCol">
+              <h1 id="dashboard_h1"><span id="welcome">Welcome  </span><span id="dash_username">{this.props.username}!</span></h1>
+          </Col>
+        </Row>
+        </div>
+      </div>
+    )
+  }
+}
 
+function mapStateToProps(state) {
+  return { username: state.auth.currentUser.email }
+}
 
-export default Dashboard
+export default connect(mapStateToProps)(Dashboard)
