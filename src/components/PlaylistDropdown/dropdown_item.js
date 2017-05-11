e127d1a8a94d9b6937dd9f8426a1f4137fa7d925
Mdjurdj1 <github email address>
2017-05-10 23:44:35 -0400

    Dropdown items are now stateful and can add/delete on click

--- src/components/PlaylistDropdown/dropdown_item.js
+++ src/components/PlaylistDropdown/dropdown_item.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import { Glyphicon } from 'react-bootstrap'
 
-import {createPlaylistVideo} from '../../redux/modules/PlaylistVideos/actions'
+import {createPlaylistVideo, deleteVid} from '../../redux/modules/PlaylistVideos/actions'
 
 
 class DropdownItem extends Component {
@@ -13,10 +13,21 @@ class DropdownItem extends Component {
     }
   }
 
-  handleClick(e, p) {
-    let id = p.id
+  handleClick(e, playlist) {
+    let playlist_id = playlist.id
     let video = this.props.video
-    this.props.createPlaylistVideo(id, video)
+    let video_id = video.id
+    if(this.state.clicked === false) {
+      this.props.createPlaylistVideo(playlist_id, video)
+      this.setState({
+        clicked: true
+      })
+    } else if(this.state.clicked === true) {
+      this.props.deleteVid(playlist_id, video_id)
+      this.setState({
+        clicked: false
+      })
+    }
   }
 
   render() {
@@ -24,11 +35,14 @@ class DropdownItem extends Component {
     const index = this.props.key
     return (
       <li key={index} onClick={(e)=>{this.handleClick(e, p)}}>
-        <Glyphicon id="checked" glyph="heart-empty"/>&nbsp;&nbsp; {p.name}
+        { this.state.clicked === false ?
+            <span><Glyphicon className="add_to_playlist_glyph" glyph="heart-empty"/>&nbsp;&nbsp; {p.name}</span> :
+            <span><Glyphicon className="add_to_playlist_glyph" glyph="check"/>&nbsp;&nbsp; {p.name}</span>
+           }
         <hr id="menu_divider"/>
       </li>
     )
   }
 }
 
-export default connect(null, {createPlaylistVideo})(DropdownItem)
+export default connect(null, {createPlaylistVideo, deleteVid})(DropdownItem)
