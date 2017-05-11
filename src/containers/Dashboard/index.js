8a35d4ba5ee1e23e91a1f9e6eb0b19820aaeda4c
Mdjurdj1 <github email address>
2017-05-11 04:10:52 -0400

    Added some text to dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -8,12 +8,23 @@ class Dashboard extends Component {
     return (
       <div>
         <div className="banner">
-        <Row>
-          <Col xs={12} md={12} id="bufferCol">
-              <h1 id="dashboard_h1"><span id="welcome">Welcome  </span><span id="dash_username">{this.props.username}!</span></h1>
-          </Col>
-        </Row>
+          <Row>
+            <Col xs={12} md={12} id="bufferCol">
+                <h1 id="dashboard_h1"><span id="welcome">Welcome  </span><span id="dash_username">{this.props.username}!</span></h1>
+            </Col>
+          </Row>
         </div>
+
+        <div className="dashboard_container">
+          <Row>
+            <Col md={4} mdOffset={4}>
+              <h1 id="get_started">Let's get started!</h1>
+              <p>Search for videos using the navbar</p>
+              <p>Create playlists</p>
+              <p>Save songs to your custom playlists!</p>
+            </Col>
+          </Row>
+          </div>
       </div>
     )
   }
