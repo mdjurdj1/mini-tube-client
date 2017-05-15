983e1c9f59a008ea8383503a58edeef419a3df9a
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-15 11:58:06 -0400

    Changed youtube search yield to 7 results

--- src/redux/modules/Video/actions.js
+++ src/redux/modules/Video/actions.js
@@ -15,7 +15,7 @@ export function searchVids(query) {
     q: query,
     key: key,
     type: `video`,
-    maxResults: 10,
+    maxResults: 7,
     videoEmbeddable: `true`
   }
 
--- src/redux/modules/Video/reducer.js
+++ src/redux/modules/Video/reducer.js
@@ -3,7 +3,7 @@ export default function videosReducer(state={loading: false, links: []}, action)
     case 'LOADING_VIDS':
       return {...state, loading: true}
     case 'FETCH_VIDS':
-      return {...state, loading: false, links: action.payload}
+      return {...state, loading: false, links: action.payload }
     case 'FETCH_VID':
       return {...state, loading: false, video: action.payload }
     default:
