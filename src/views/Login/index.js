d6d741ccb25d6aa3d050271dbb7079ca4eb77827
Mdjurdj1 <mdjurdj1@gmail.com>
2017-05-15 22:54:24 -0400

    Added load indicator to login

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -25,29 +25,56 @@ class Login extends Component {
 
   handleLogin = data => this.props.login({ user: data }, this.context.router);
 
+
   render() {
+
+      let content = null;
+      if(this.props.authenticating) {
+        content =
+          <Row>
+            <Col sm={4} md={4} />
+            <Col sm={4} md={4} id="login_box">
+              <h1 id="login_header">Log in to Mini-Tube</h1>
+              { this.props.errors ? <ErrorAlert /> : null }
+              <LoginForm onSubmit={this.handleLogin} />
+              <div className="loader"></div>
+              <hr id="blue_login_hr" />
+
+              <div className="already_reg">
+                <p>Dont have an account?</p>
+                <NavLink to="/signup" id="signup_link"><Glyphicon glyph="share-alt" />  Create new account </NavLink>
+              </div>
+            </Col>
+          </Row>
+      } else {
+        content =
+          <Row>
+            <Col sm={4} md={4} />
+            <Col sm={4} md={4} id="login_box">
+              <h1 id="login_header">Log in to Mini-Tube</h1>
+              { this.props.errors ? <ErrorAlert /> : null }
+              <LoginForm onSubmit={this.handleLogin} />
+
+              <hr id="blue_login_hr" />
+
+              <div className="already_reg">
+                <p>Dont have an account?</p>
+                <NavLink to="/signup" id="signup_link"><Glyphicon glyph="share-alt" />  Create new account </NavLink>
+              </div>
+            </Col>
+          </Row>
+      }
+
     return (
-      <Row>
-        <Col sm={4} md={4} />
-        <Col sm={4} md={4} id="login_box">
-          <h1 id="login_header">Log in to Mini-Tube</h1>
-          { this.props.errors ? <ErrorAlert /> : null }
-          <LoginForm onSubmit={this.handleLogin} />
-
-          <hr id="blue_login_hr" />
-
-          <div className="already_reg">
-            <p>Dont have an account?</p>
-            <NavLink to="/signup" id="signup_link"><Glyphicon glyph="share-alt" />  Create new account </NavLink>
-          </div>
-        </Col>
-      </Row>
+      <div>
+      {content}
+      </div>
     )
   }
 }
 
 function mapStateToProps(state) {
-  return { errors: state.auth.errors }
+  return { errors: state.auth.errors, authenticating: state.auth.isAuthenticating }
 }
 
 export default connect(mapStateToProps, { login, resetErrors })(Login);
